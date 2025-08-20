f5adf37030456fad2bdaf8261d7541ac2b4a916b
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-08-20 09:07:37 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import PostFeed from "@/components/PostFeed";
+import Sidebar from "@/components/Sidebar";
+import SignUpPrompt from "@/components/SignUpPrompt";
+import Widgets from "@/components/Widgets";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-[#0F1419] min-h-screen 
+      max-w-[1400px] mx-auto border-2 border-black
+      flex
+      
+      ">
+        <Sidebar />
+        <PostFeed />
+        <Widgets />
+
+      </div>
+      <SignUpPrompt />
+    </>
+  );
+}
---
+++ components/SignUpPrompt.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function SignUpPrompt() {
+  return (
+    <>
+    <div className='fixed bottom-0 flex justify-center items-center w-full h-[80px] bg-[#F4AF01] md:space-x-5
+    lg:justify-between lg:px-20 xl:px-40 2xp:px-80
+    '>
+        
+        <div className='hidden md:flex flex-col text-white  '>
+            <span className='text-xl font-bold ' >Don't miss out on the buzz</span>
+            <span>People on Busy Bee are always the first to know.</span>
+        </div>
+
+        <div className='flex space-x-2 w-full md:w-fit p-3  '>
+            <button className='w-full h-[48px] md:w-[88px] md:h-[40px] text-md md:text-sm border-2 border-gray-100 rounded-full text-white font-bold hover:bg-white hover:bg-opacity-25 transition    '>Log In</button>
+            <button className='w-full h-[48px] md:w-[88px] md:h-[40px] text-md md:text-sm border-2 border-white bg-white rounded-full font-bold hover:bg-gray-100 transition    '>Sign In</button>
+        </div>
+        
+    </div>
+      
+    </>
+  )
+}
